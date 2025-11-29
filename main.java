1d0f7ac47ae7298161daef824e0be29ca66dd40b
PedroPepin <pedro.manuel2e@gmail.com>
2025-11-29 20:59:21 +0100

    correction

--- main.java
+++ main.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame ventana = new JFrame("Ajedrez - Proyecto Videojuego");
+        JFrame ventana = new JFrame("Ajedrez -Proyecto Videojuego");
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setSize(800, 800);
         ventana.setLayout(new BorderLayout());
