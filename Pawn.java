809000bc457f1fa1072bb7825f30ccc08732e057
Patricia <patricia.onaney@gmail.com>
2025-12-14 23:53:28 +0100

    Actualizar

--- Pawn.java
+++ Pawn.java
@@ -1,21 +1,21 @@
+import java.awt.Color;//Para poder usar en los atributos colores tengo que usar esta biblioteca
+
 public class Pawn extends ChessPiece {
 
     // Constructor del Peón
-    public Pawn(Color colorPieza, int x, int y) {
+    public Pawn(Color colorpieza, int x, int y) {
         // Llamamos al constructor del padre.
-        // Fíjate que pasamos "Pawn" automáticamente como tipología.
-        super("Pawn", colorPieza, x, y);
+        // Pasamos "Pawn" automáticamente como tipología. Es un String. 
+        super("Pawn", colorpieza, x, y);
     }
 
     @Override
     public boolean validarMovimiento(int nuevaX, int nuevaY) {
-        // AQUI VA LA LÓGICA DEL AJEDREZ
-        // El peón es complicado porque depende del color (avanza hacia arriba o abajo)
-        
-        // Ejemplo básico: El peón solo se mueve "hacia adelante" en el eje Y.
+        // El peón  depende del color (avanza hacia arriba o abajo)
+        //El peón solo se mueve "hacia adelante" en las filas.
         // Asumiendo que las blancas avanzan hacia Y positivo y negras a Y negativo.
         
-        int direccion = (this.colorPieza == Color.BLANCO) ? 1 : -1;
+        int direccion = (this.colorpieza == Color.WHITE) ? 1 : -1;
 
         // 1. Verificar que no se mueve en X (a menos que coma, que es otra lógica)
         if (this.x != nuevaX) {
@@ -27,7 +27,6 @@ public class Pawn extends ChessPiece {
             return true;
         }
 
-        // (Falta lógica de primer movimiento doble, pero esto es lo básico)
         return false;
     }
 }
