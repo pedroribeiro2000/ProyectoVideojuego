b3518a592dc503245fbfb1dfcd722123ae1a54ac
Cristina <cristina.garcia.andr@gmail.com>
2026-01-07 21:04:09 +0100

    Add Knight piece and implement legal move logic

--- Knight.java
+++ Knight.java
@@ -1,7 +1,43 @@
 package ProyectoVideojuego;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Knight extends Chesspiece {
     public Knight(int row, int col, boolean isWhite) {
         super(row, col, isWhite, isWhite ? "knight-white.png" : "knight-black.png");
+    }
+
+    /**
+     * Movimientos legales básicos del caballo:
+     * - Se mueve en "L" (2+1) y puede saltar piezas.
+     * - Puede capturar una pieza rival en la casilla destino.
+     *
+     * Convención: Point.x = col, Point.y = row.
+     */
+    List<Point> getLegalMoves(Board board) {
+        List<Point> moves = new ArrayList<>();
+
+        int[][] jumps = {
+                {-2, -1}, {-2,  1},
+                {-1, -2}, {-1,  2},
+                { 1, -2}, { 1,  2},
+                { 2, -1}, { 2,  1}
+        };
+
+        for (int[] j : jumps) {
+            int r = row + j[0];
+            int c = col + j[1];
+
+            if (!board.isInsideBoard(r, c)) continue;
+
+            Chesspiece target = board.getPieceAt(r, c);
+            if (target == null || target.isWhite() != this.isWhite) {
+                moves.add(new Point(c, r));
+            }
+        }
+
+        return moves;
     }
 }
